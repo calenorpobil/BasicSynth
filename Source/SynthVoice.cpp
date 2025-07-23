c81b92f8b6643a103973a82b5568476611af3570
Calitos <89901767+calenorpobil@users.noreply.github.com>
2025-07-23 10:46:45 +0200

    Start

---
+++ Source/SynthVoice.cpp
@@ -0,0 +1,69 @@
+/*
+  ==============================================================================
+
+    SynthVoice.cpp
+    Created: 19 Jul 2025 9:49:41am
+    Author:  Carlos
+
+  ==============================================================================
+*/
+
+#include "SynthVoice.h"
+
+
+bool SynthVoice::canPlaySound(juce::SynthesiserSound* sound)
+{
+    return dynamic_cast<juce::SynthesiserSound*>(sound) != nullptr;
+}
+
+void SynthVoice::startNote(int midiNoteNumber, float velocity, juce::SynthesiserSound* sound, int currentPitchWheelPosition)
+{
+    osc.setFrequency(juce::MidiMessage::getMidiNoteInHertz(midiNoteNumber));
+    adsr.noteOn();
+}
+
+void SynthVoice::stopNote(float velocity, bool allowTailOff)
+{
+    adsr.noteOff();
+}
+
+void SynthVoice::controllerMoved(int controllerNumber, int newControllerValue)
+{
+
+}
+
+void SynthVoice::pitchWheelMoved(int newPitchWheelValue)
+{
+
+}
+
+void SynthVoice::prepareToPlay(double sampleRate, int samplesPerBlock, int outputChannels)
+{
+    adsr.setSampleRate(sampleRate);
+    
+    juce::dsp::ProcessSpec spec;
+    spec.sampleRate = sampleRate;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.numChannels = outputChannels;
+
+    osc.prepare(spec);
+    gain.prepare (spec);
+
+    //osc.setFrequency (440.0f); // Set frequency to 440 Hz (A4)
+    gain.setGainLinear (0.5f); // Set gain to 0.5 (50% volume)
+
+    isPrepared = true;
+    
+}
+
+
+void SynthVoice::renderNextBlock(juce::AudioBuffer< float >& outputBuffer, int startSample, int numSamples)
+{
+    jassert(isPrepared);
+
+    juce::dsp::AudioBlock<float> audioBlock{ outputBuffer };
+    osc.process(juce::dsp::ProcessContextReplacing<float>(audioBlock));
+    gain.process(juce::dsp::ProcessContextReplacing<float>(audioBlock));
+
+    adsr.applyEnvelopeToBuffer(outputBuffer, startSample, numSamples);
+}
